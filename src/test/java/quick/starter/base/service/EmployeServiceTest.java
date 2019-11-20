5c351dd73aec1df0303594a4eabb2dd912c4dc21
Renaud Trezieres <r.trezieres@alltech.local>
2019-11-20 14:09:33 +0100

    add tu employeService

--- src/test/java/quick/starter/base/service/EmployeServiceTest.java
+++ src/test/java/quick/starter/base/service/EmployeServiceTest.java
@@ -1,31 +1,51 @@
 package quick.starter.base.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import quick.starter.base.controller.mapper.EmployeMapper;
+import quick.starter.base.controller.resource.EmployeResource;
+import quick.starter.base.jpa.Employe;
 import quick.starter.base.jpa.EmployeRepository;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class EmployeServiceTest
 {
 
-    @Autowired
+    @InjectMocks
     private EmployeService service;
 
     @Mock
     private EmployeRepository repository;
 
+    @Mock
+    private EmployeMapper mapper;
+
     @Test
     public void testGetAllEmploye() throws Exception
     {
-        // when(this.repository)
-
-        this.service.getAllEmploye();
+        // Arrange
+        final List<Employe> lstEmploye = Lists.newArrayList();
+        final List<EmployeResource> lstEmployeResource = Lists.newArrayList();
+        when(this.repository.findAll()).thenReturn(lstEmploye);
+        when(this.mapper.listEmployeToListEmployeResource(lstEmploye)).thenReturn(lstEmployeResource);
+        // Act
+        final List<EmployeResource> result = this.service.getAllEmploye();
+        // Assert
+        assertEquals(result, lstEmploye);
+        verify(this.mapper, only()).listEmployeToListEmployeResource(lstEmploye);
+        verify(this.repository, only()).findAll();
 
     }
 
